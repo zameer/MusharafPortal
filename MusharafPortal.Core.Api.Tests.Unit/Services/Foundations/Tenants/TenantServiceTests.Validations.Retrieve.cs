a72710e9a6389a00ea14cc59411709c6f39ff37f
Ahamed Zameer <ahamed.zameer@osos.om>
2022-11-17 18:59:43 +0530

    ShouldThrowValidationExceptionOnRetrieveWhenStorageTenantIsNullAndLogAsync - PASS

--- MusharafPortal.Core.Api.Tests.Unit/Services/Foundations/Tenants/TenantServiceTests.Validations.Retrieve.cs
+++ MusharafPortal.Core.Api.Tests.Unit/Services/Foundations/Tenants/TenantServiceTests.Validations.Retrieve.cs
@@ -39,5 +39,36 @@ namespace MusharafPortal.Core.Api.Tests.Unit.Services.Foundations.Tenants
 
             this.storageBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnRetrieveWhenStorageTenantIsNullAndLogAsync()
+        {
+            // given
+            Guid randonTenantId = Guid.NewGuid();
+            Guid inputTenantId = randonTenantId;
+            Tenant nullStorageTenant = null;
+
+            var notFoundTenantException = new NotFoundTenantException(inputTenantId);
+
+            var expectedTenantValidationException = new TenantValidationException(notFoundTenantException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectTenantByIdAsync(inputTenantId))
+                    .ReturnsAsync(nullStorageTenant);
+
+            // when
+            ValueTask<Tenant> retrieveTenantByIdTask =
+                this.tenantService.RetreiveTenantByIdAsync(inputTenantId);
+
+            // then
+            await Assert.ThrowsAsync<TenantValidationException>(() => retrieveTenantByIdTask.AsTask());
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectTenantByIdAsync(It.IsAny<Guid>()),
+                Times.Once
+            );
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
