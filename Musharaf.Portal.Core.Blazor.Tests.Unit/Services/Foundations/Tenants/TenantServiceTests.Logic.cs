f64929ea60e789cc9722582a720efd6bf690de1b
Ahamed Zameer <ahamed.zameer@osos.om>
2022-11-18 19:44:56 +0530

    ShouldCreateTenantAsync -> FAIL

---
+++ Musharaf.Portal.Core.Blazor.Tests.Unit/Services/Foundations/Tenants/TenantServiceTests.Logic.cs
@@ -0,0 +1,39 @@
+﻿using FluentAssertions;
+using Moq;
+using Musharaf.Portal.Core.Blazor.Models.Tenants;
+
+namespace Musharaf.Portal.Core.Blazor.Tests.Unit.Services.Foundations.Tenants
+{
+    public partial class TenantServiceTests
+    {
+        [Fact]
+        public async Task ShouldCreateTenantAsync()
+        {
+            // given
+            Tenant randomTenant = CreateRandomTenant();
+            Tenant inputTenant = randomTenant;
+            Tenant retrievedTenant = inputTenant;
+            Tenant expectedTenant = retrievedTenant;
+
+            this.apiBrokerMock.Setup(broker =>
+                broker.PostTenantAsync(inputTenant))
+                .ReturnsAsync(retrievedTenant);
+
+            // when
+            Tenant actualTenant = 
+                await this.tenantService
+                .CreateTenantAsync(inputTenant);
+
+            // then
+            actualTenant.Should().BeEquivalentTo(expectedTenant);
+
+            this.apiBrokerMock.Verify(broker =>
+                broker.PostTenantAsync(inputTenant), 
+                Times.Once());
+
+            this.apiBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+
+    }
+}
--- Musharaf.Portal.Core.Blazor.Tests.Unit/Services/Foundations/Tenants/TenantServiceTests.cs
+++ Musharaf.Portal.Core.Blazor.Tests.Unit/Services/Foundations/Tenants/TenantServiceTests.cs
@@ -1,11 +1,13 @@
 ﻿using Moq;
 using Musharaf.Portal.Core.Blazor.Brokers.Apis;
 using Musharaf.Portal.Core.Blazor.Brokers.Loggings;
+using Musharaf.Portal.Core.Blazor.Models.Tenants;
 using Musharaf.Portal.Core.Blazor.Services.Foundations.Tenants;
+using Tynamix.ObjectFiller;
 
 namespace Musharaf.Portal.Core.Blazor.Tests.Unit.Services.Foundations.Tenants
 {
-    public class TenantServiceTests
+    public partial class TenantServiceTests
     {
         private readonly Mock<IApiBroker> apiBrokerMock;
         private readonly Mock<ILoggingBroker> loggingBrokerMock;
@@ -13,12 +15,25 @@ namespace Musharaf.Portal.Core.Blazor.Tests.Unit.Services.Foundations.Tenants
 
         public TenantServiceTests()
         {
-            this.apiBrokerMock= new Mock<IApiBroker>();
-            this.loggingBrokerMock= new Mock<ILoggingBroker>();
+            this.apiBrokerMock = new Mock<IApiBroker>();
+            this.loggingBrokerMock = new Mock<ILoggingBroker>();
             this.tenantService = new TenantService(
                     apiBroker: this.apiBrokerMock.Object,
                     loggingBroker: this.loggingBrokerMock.Object
                 );
+        }
+
+        private static Tenant CreateRandomTenant() =>
+            CreateTenantFiller().Create();
+
+        private static Filler<Tenant> CreateTenantFiller()
+        {
+            var filler = new Filler<Tenant>();
+
+            filler.Setup()
+                .OnType<DateTimeOffset>().Use(DateTimeOffset.UtcNow);
+
+            return filler;
         }
     }
 }
