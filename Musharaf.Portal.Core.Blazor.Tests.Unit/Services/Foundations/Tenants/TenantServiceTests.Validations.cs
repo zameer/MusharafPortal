39be209df8eeeba1bb67dc50165185725ba3ca16
Ahamed Zameer <ahamed.zameer@osos.om>
2022-11-19 11:32:09 +0530

    ShouldThrowValidationExceptionOnCreateIfCreatedByIsInvalidAndLogItAsync -> FAIL

--- Musharaf.Portal.Core.Blazor.Tests.Unit/Services/Foundations/Tenants/TenantServiceTests.Validations.cs
+++ Musharaf.Portal.Core.Blazor.Tests.Unit/Services/Foundations/Tenants/TenantServiceTests.Validations.cs
@@ -112,5 +112,42 @@ namespace Musharaf.Portal.Core.Blazor.Tests.Unit.Services.Foundations.Tenants
             this.loggingBrokerMock.VerifyNoOtherCalls();
             this.apiBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Fact]
+
+        public async Task ShouldThrowValidationExceptionOnCreateIfCreatedByIsInvalidAndLogItAsync()
+        {
+            // given
+            Guid invalidId = Guid.Empty;
+            Tenant randomTenant = CreateRandomTenant();
+            Tenant invalidTenant = randomTenant;
+            invalidTenant.CreatedBy = invalidId;
+
+            var invalidTenantException =
+                new InvalidTenantException(
+                    parameterName: nameof(Tenant.CreatedBy),
+                    parameterValue: invalidTenant.CreatedBy);
+
+            var expectedTenantValidationException = new TenantValidationException(invalidTenantException);
+
+            // when
+            ValueTask<Tenant> createTenantTask =
+                this.tenantService.CreateTenantAsync(invalidTenant);
+
+            // then
+            await Assert.ThrowsAsync<TenantValidationException>(() =>
+                createTenantTask.AsTask());
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.IsAny<TenantValidationException>()),
+                    Times.Once);
+
+            this.apiBrokerMock.Verify(broker =>
+                broker.PostTenantAsync(It.IsAny<Tenant>()),
+                Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.apiBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
